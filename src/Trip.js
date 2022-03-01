12ce85e85d8f8806db0ee78c174afbb756d0567d
Kim Ward <kmewrd@outlook.com>
2022-03-01 16:19:12 -0600

    Write code to pass all Trip tests

--- src/Trip.js
+++ src/Trip.js
@@ -1,6 +1,13 @@
 class Trip {
-  constructor() {
-
+  constructor(data) {
+    this.id = data.id;
+    this.userID = data.userID;
+    this.destinationID = data.destinationID;
+    this.travelers = data.travelers;
+    this.date = data.date;
+    this.duration = data.duration;
+    this.status = data.status;
+    this.suggestedActivities = data.suggestedActivities;
   }
 }
 
