cde5a9e6ea3f5438f5b3b3fcdf90e1061183d98c
Kim Ward <kmewrd@outlook.com>
2022-03-01 17:55:06 -0600

    Add class imports to scripts file

--- src/scripts.js
+++ src/scripts.js
@@ -1,3 +1,6 @@
 import './css/base.scss';
 import './images/turing-logo.png';
 import {fetchData, postData} from './apiCalls';
+import Traveler from './Traveler';
+import Destination from './Destination';
+import Trip from './Trip';
