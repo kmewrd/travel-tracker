4e3205241b3c0fd9dd450b3fb4f56d8aca01fa79
Kim Ward <kmewrd@outlook.com>
2022-03-01 15:44:43 -0600

    Write remaining tests for class properties, fail tests, and pass tests

--- src/Traveler.js
+++ src/Traveler.js
@@ -1,5 +1,9 @@
 class Traveler {
-
+  constructor(data) {
+    this.id = data.id;
+    this.name = data.name;
+    this.travelerType = data.travelerType;
+  }
 }
 
 export default Traveler;
--- test/Traveler-test.js
+++ test/Traveler-test.js
@@ -14,13 +14,25 @@ describe('Traveler', () => {
     traveler1 = new Traveler(travelerData[0]);
     traveler2 = new Traveler(travelerData[1]);
     traveler3 = new Traveler(travelerData[2]);
-  });
+  })
 
   it('should be a function', () => {
     expect(Traveler).to.be.a('function');
-  });
+  })
 
   it('should be an instance of Traveler', () => {
     expect(traveler1).to.be.an.instanceof(Traveler);
-  });
+  })
+
+  it('should have an id', () => {
+    expect(traveler1.id).to.equal(1);
+  })
+
+  it('should have a name', () => {
+    expect(traveler2.name).to.equal('Rachael Vaughten');
+  })
+
+  it('should have a type', () => {
+    expect(traveler3.travelerType).to.equal('shopper');
+  })
 });
