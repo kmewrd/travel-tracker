732493ec5a6ae0b1c8572a677ad97ae425186bed
Kim Ward <kmewrd@outlook.com>
2022-03-01 16:00:30 -0600

    Write remaining tests to check for properties of Destination

--- test/Destination-test.js
+++ test/Destination-test.js
@@ -19,4 +19,32 @@ describe('Destination', () => {
   it('should be a function', () => {
     expect(Destination).to.be.a('function');
   })
+
+  it('should be an instance of Destination', () => {
+    expect(destination2).to.be.an.instanceof(Destination);
+  })
+
+  it('should have an id', () => {
+    expect(destination1.id)to.equal(7);
+  })
+
+  it('should have a location', () => {
+    expect(destination1.location).to.equal('Paris, France');
+  })
+
+  it('should have an estimated lodging cost per day', () => {
+    expect(destination1.estimatedLodgingCostPerDay).to.equal(100);
+  })
+
+  it('should have an estimated flight cost per person', () => {
+    expect(destination1.estimatedFlightCostPerPerson).to.equal(395);
+  })
+
+  it('should have an image URL', () => {
+    expect(destination2.image).to.equal('https://images.unsplash.com/photo-1534351590666-13e3e96b5017?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80');
+  })
+
+  it('should have alt text for the image', () => {
+    expect(destination2.alt).to.equal('canal with boats and trees and buildings along the side');
+  })
 });
