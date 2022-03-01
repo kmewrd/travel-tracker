b35b5a7425cd83d44aca1ba541c6d01a8341b8e8
Kim Ward <kmewrd@outlook.com>
2022-03-01 16:15:40 -0600

    Write remaining tests for properties on Trip

--- test/Trip-test.js
+++ test/Trip-test.js
@@ -19,4 +19,40 @@ describe('Trip', () => {
   it('should be a function', () => {
     expect(Trip).to.be.a('function');
   })
+
+  it('should be an instance of Trip', () => {
+    expect(trip1).to.be.an.instanceof(Trip);
+  })
+
+  it('should have an id', () => {
+    expect(trip1.id).to.equal(3);
+  })
+
+  it('should keep track of which user requested the trip', () => {
+    expect(trip2.userID).to.equal(3);
+  })
+
+  it('should keep track of the trip destination', () => {
+    expect(trip2.destinationID).to.equal(25)
+  })
+
+  it('should keep track of the number of travelers on the trip', () => {
+    expect(trip1.travelers).to.equal(4);
+  })
+
+  it('should have a date', () => {
+    expect(trip3.date).to.equal('2021/01/09');
+  })
+
+  it('should have a duration in days', () => {
+    expect(trip3.duration).to.equal(15);
+  })
+
+  it('should keep track of its approval status', () => {
+    expect(trip3.status).to.equal('approved');
+  })
+
+  it('should be able to hold suggested activities', () => {
+    expect(trip1.suggestedActivities).to.deep.equal([]);
+  })
 });
