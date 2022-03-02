c816af42240b8b117613e22594b2623bbcfd7329
Kim Ward <kmewrd@outlook.com>
2022-03-02 12:16:43 -0600

    Add global variables and start writing first function

--- src/scripts.js
+++ src/scripts.js
@@ -3,3 +3,15 @@ import {fetchData, postData} from './apiCalls';
 import Traveler from './Traveler';
 import Destination from './Destination';
 import Trip from './Trip';
+
+// global variables
+let traveler;
+let trips;
+let destinations;
+
+// functions
+function initializeUserData(travelerId, tripsData) {
+  
+}
+
+// event listeners
