650cc73bbee5a0550e2cbe779d79e1601bbf39f7
Kim Ward <kmewrd@outlook.com>
2022-03-02 17:04:05 -0600

    Delete console logs and refactor to return correctly formatted date

--- src/utils.js
+++ src/utils.js
@@ -1,14 +1,10 @@
 const helperFunctions = {
   getTodayDate() {
     const today = new Date();
-    console.log(today);
     const mm = (today.getMonth() + 1).toString().padStart(2, '0');
-    console.log(mm);
     const dd = today.getDate().toString().padStart(2, '0');
-    console.log(dd);
     const yyyy = today.getFullYear().toString();
-    console.log(yyyy);
-    return today;
+    return `${yyyy}/${mm}/${dd}`;
   }
 };
 
