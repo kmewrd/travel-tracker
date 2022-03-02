4be98bc37e435e820caa40b38a96413f17ff009d
Kim Ward <kmewrd@outlook.com>
2022-03-02 16:14:52 -0600

    Add query selectors for elements to udpate on DOM

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,4 +1,8 @@
 const travelerName = document.getElementById('travelerName');
+const upcomingTrips = document.querySelector('.js-upcoming-trips');
+const pendingTrips = document.querySelector('.js-pending-trips');
+const pastTrips = document.querySelector('.js-past-trips');
+const travelExpenses = document.querySelector('.js-travel-expenses');
 
 const domUpdates = {
 
