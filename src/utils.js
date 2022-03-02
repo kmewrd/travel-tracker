4ce984deac114d75284a513f4c944f49c848433e
Kim Ward <kmewrd@outlook.com>
2022-03-02 17:02:58 -0600

    Write lines that get month and year as string of nums

--- src/utils.js
+++ src/utils.js
@@ -2,8 +2,12 @@ const helperFunctions = {
   getTodayDate() {
     const today = new Date();
     console.log(today);
+    const mm = (today.getMonth() + 1).toString().padStart(2, '0');
+    console.log(mm);
     const dd = today.getDate().toString().padStart(2, '0');
     console.log(dd);
+    const yyyy = today.getFullYear().toString();
+    console.log(yyyy);
     return today;
   }
 };
